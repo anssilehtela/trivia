0863a54644c0b94754023f70d6b3630dc3902f39
Anssi <anssi@HD695-ANSSIL.local>
2021-03-08 16:29:45 +0200

    Switch to use case

--- ruby/spec/game_spec.rb
+++ ruby/spec/game_spec.rb
@@ -8,21 +8,20 @@ describe "game" do
   context 'when adding players' do
     it 'allows adding valid player' do
       expect(game.add_player("player 1")).to be true
-      expect(game.how_many_players).to eq 1
+      expect(game.players.length).to eq 1
     end
 
     it 'does not allow adding more than 6 players' do
       6.times {|k| game.add_player("player #{k}") }
-      expect(game.how_many_players).to eq 6
+      expect(game.players.length).to eq 6
       expect(game.add_player("seventh player")).to be false
-      expect(game.how_many_players).to eq 6
+      expect(game.players.length).to eq 6
     end
   end
 
   context "when asking for category" do
-    it 'returns correct one based on place on board' do
-      binding.pry
-      expect(game.current_category).to eq "aef"
+    it 'returns correct one based on players place on board' do
+      expect(game.current_category).to eq "Pop"
     end
   end
 
